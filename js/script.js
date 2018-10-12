57e55c12ef78f5ec4911d512805d17775dfaeae3
辻ひかる <hikaru848tuji@gmail.com>
2018-10-12 20:51:33 +0900

    最初のコミット

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+var script = document.createElement('script');
+
+script.src =;
+document.body.appendChild(script);
+document.body.removeChild(script);
+function jsonData(data) {
+
+    console.log(data);
+
+}
