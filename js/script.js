82cd621fcee88e6e39d41dd3cd04982d7d2eed7c
辻ひかる <hikaru848tuji@gmail.com>
2018-10-13 19:59:44 +0900

    (テスト)instagram apiを使ったデータの取得

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
+$(document).ready(function(){
+  var baseURL = 'https://api.instagram.com/v1/users/self/media/recent';
+  var imgData;
 
-var script = document.createElement('script');
+  var getData = function(url){
+    $.ajax({
+      url: url,
+      dataType: 'jsonp',
+      data: {
+        access_token:'',
+        count: 12
+      }
+    })
 
-script.src =;
-document.body.appendChild(script);
-document.body.removeChild(script);
-function jsonData(data) {
-
-    console.log(data);
-
-}
+    .done(function(data){
+      imgData = data;
+      console.dir(imgData);
+    })
+    .fail(function(data){
+      $('#gallery').text(textStatus);
+    })
+  }
+  getData = (baseURL);
+});
