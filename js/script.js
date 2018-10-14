c8d9c3cd073285d544078df0562079022620a0b8
辻ひかる <hikaru848tuji@gmail.com>
2018-10-14 18:42:04 +0900

    instagramから所得した画像を表示

--- js/script.js
+++ js/script.js
@@ -15,6 +15,26 @@ $(document).ready(function(){
     .done(function(data){
       imgData = data;
       console.dir(imgData);
+
+      $(imgData.data).each(function(){
+
+        var caption ='';
+        if(this.caption){
+          caption = this.caption.text;
+        }
+
+        $('#gallery').append(
+          $('<div class="img_block"></div>').append(
+            $('<a></a>')
+            .attr('href', this.link)
+            .attr('target', '_blank').append(
+              $('<img>').attr('src',this.images.low_resolution.url)
+            )
+          )
+
+        );
+
+      });
     })
     .fail(function(data){
       $('#gallery').text(textStatus);
