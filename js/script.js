1bff857002fea12559b387b061d6df3373920434
辻ひかる <hikaru848tuji@gmail.com>
2018-10-15 17:16:56 +0900

    [add] youtube api のコードを追加
    youtube data api v3 を使ったyoutubeのデータの取得を追加した

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
 $(document).ready(function(){
+  
+  var youtubeURL ='https://www.googleapis.com/youtube/v3/search';
+  var youtubeImgData;
+
   var instaURL = 'https://api.instagram.com/v1/users/self/media/recent';
   var instaImgData;
+
+
+  var getYoutubeData = function(url){
+    $.ajax({
+      url: url,
+      dataType: 'jsonp',
+
+      data: {
+        part: 'id',
+        channelId:youtube_userName,
+        order: 'date',
+        key: youtube_APIkey,
+      }
+    })
+    .done(function(data){
+      youtubeImgData = data;
+      console.dir(youtubeImgData);
+  })
+  .fail(function(data){
+    console.log('youtube_failed');
+  })
+}
 
   var getInstaData = function(url){
     $.ajax({
@@ -39,6 +65,8 @@ $(document).ready(function(){
     .fail(function(data){
       console.log('instagram_failed');
   }
+
+  getYoutubeData(youtubeURL);
   getInstaData(instaURL);
 
 });
