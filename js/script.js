bca7d17bd5855d242b58a9500caa4ca39bc4ccb6
辻ひかる <hikaru848tuji@gmail.com>
2018-10-19 17:30:40 +0900

    [update] ES6に書き換え
    ES6に書き換えた、each文をfor文に変更した

--- js/script.js
+++ js/script.js
@@ -1,93 +1,76 @@
 $(document).ready(function(){
 
-  {
-      const chanelURL =`https://www.youtube.com/channel/${youtube_userName}/videos`;
-      $('#yt-btn').append(
-        $('<a class="btn-content">Youtubeへ</a>')
-        .attr('href',chanelURL)
-        .attr('target', '_blank'));
-  }
+  const YOUTUBE_SEARCH_API_URL ='https://www.googleapis.com/youtube/v3/search';
+  const INSTAGRAM_RECENT_API_URL = 'https://api.instagram.com/v1/users/self/media/recent';
+  const YOUTUBE_CHANNEL_ID = 'UCifqUptiSNtp1u8ma9LryGA';
 
-  {
-    const instaMypageURL =`https://www.instagram.com/${instagram_userName}`;
-    $('#inst-btn').append(
-      $('<a class="btn-content">Instagramへ</a>')
-      .attr('href',instaMypageURL)
-      .attr('target', '_blank'));
-  }
-
-  $('#copyRight').append(copyRight);
-
-  let youtubeURL ='https://www.googleapis.com/youtube/v3/search';
-  let instaURL = 'https://api.instagram.com/v1/users/self/media/recent';
-
-
-  const getYoutubeData = function(url){
+  const getYoutubeData = (_channelId,_maxResults) =>{
     $.ajax({
-      url: url,
-      dataType: 'jsonp',
+      url: YOUTUBE_SEARCH_API_URL,
+      dataType: 'json',
       data: {
         part: 'id',
-        channelId:youtube_userName,
+        channelId: _channelId,
         order: 'date',
-        maxResults: 3,
-        key: youtube_APIkey,
+        maxResults: _maxResults,
+        key: YOUTUBE_API_KEY,
       }
     })
-
-    .done(function(data){
+    .done((data) =>{
       const youtubeImgData = data;
-      // console.dir(youtubeImgData);
-
-      $(youtubeImgData.items).each(function(){
-
-        const videoName = this.id.videoId;
-        const iframeText  = `https://www.youtube.com/embed/${videoName}`
-        $('#movie').append(
-          $('<div class="mov_block"></div>').append(
-            $('<iframe width="640px" height="360px" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
-            .attr('src',iframeText)
-          )
-        );
-      });
+      const $movie = $('#movie');
+      let iframeHTML = '';
+      for( let i = 0, len = youtubeImgData.items.length; i < len; i++ ){
+        const item = youtubeImgData.items[i];
+        const videoId = item.id.videoId;
+        iframeHTML += `
+        <div class="mov_block">
+        <iframe
+        width="640"
+        height="360"
+        src="https://www.youtube.com/embed/${videoId}?rel=0"
+        frameborder="0"
+        allow="autoplay; encrypted-media"
+        allowfullscreen
+        ></iframe>
+      </div>
+      `;
+      }
+      $movie.append(iframeHTML);
     })
-
-    .fail(function(data){
-      console.log('youtube_failed');
-    })
+    .fail((error) =>{
+      console.error(error);
+    });
   }
 
-
-  const getInstaData = function(url){
+  const getInstaData = (_count) =>{
     $.ajax({
-      url: url,
-      dataType: 'jsonp',
+      url: INSTAGRAM_RECENT_API_URL,
+      dataType: 'json',
       data: {
-        access_token:insta_accesstoken,
-        count: 9
+        access_token:INSTAGRAM_ACCESSTOKEN,
+        count: _count
       }
     })
-
-    .done(function(data){
+    .done((data)=>{
       const instaImgData = data;
-      // console.dir(instaImgData);
-
-      $(instaImgData.data).each(function(){
-
-        $('#gallery').append(
-          $('<div class="img_block"></div>').append(
-            $('<img>').attr('src',this.images.low_resolution.url)
-          )
-        );
-      });
+      const $gallery = $('#gallery');
+      let imgHTML = '';
+      for( let i = 0, len = instaImgData.data.length; i < len; i++ ){
+        const data = instaImgData.data[i];
+        const instImg = data.images.low_resolution.url;
+        imgHTML +=`
+        <div class="img_block">
+        <img src=${instImg}>
+        </div>
+        `;
+      }
+      $gallery.append(imgHTML);
     })
-
-    .fail(function(data){
-      console.log('instagram_failed');
-    })
+    .fail((error) => {
+      console.error(error);
+    });
   }
-
-  getYoutubeData(youtubeURL);
-  getInstaData(instaURL);
-
+  getYoutubeData(YOUTUBE_CHANNEL_ID, 3);
+  getInstaData(9);
 });
